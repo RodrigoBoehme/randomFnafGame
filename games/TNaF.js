4edec015ce9160f76a5fe86afa8c736b58ef35bd
RodrigoBoehme <robi.boehme@gmail.com>
2026-03-25 18:12:39 -0300

    Miscelaneous changes on the code

--- games/TNaF.js
+++ games/TNaF.js
@@ -1,15 +1,3 @@
-const searchBtn = document.getElementById("btnSearch");
-const doorBtn = document.getElementById("btnDoor");
-const idleBtn = document.getElementById("btnIdle");
-const doorSearchBtn = document.getElementById("btnDoorSearch");
-
-// Event listeners for buttons
-searchBtn.addEventListener("click", () => (resolve = 1));
-doorBtn.addEventListener("click", () => (resolve = 2));
-idleBtn.addEventListener("click", () => (resolve = 4));
-doorSearchBtn.addEventListener("click", () => (resolve = 3));
-
-
 // Function that returns a Promise resolved when one of the buttons is clicked
 function waitForButtonClick(buttonIds) {
     return new Promise((resolve) => {
@@ -37,24 +25,36 @@ function waitForButtonClick(buttonIds) {
 }
 
 async function main() {
-  document.getElementById("result").textContent = "Waiting for your choice...";
-
   // Wait until user clicks a button
 
+  
+  fnafAlikeGame()
+  }
 
+
+async function TNaF([Bd,Fd,Fx,Cd]){
   // Continue after click
-    let Bd=5
-    let Fd=5
-    let Fx=5
-    let Cd=5
+  document.getElementById("animatronics").style.backgroundColor="rgba(128, 255, 0, 0.5)"
+  document.getElementById("btnStart").hidden=true
+  document.getElementById("btnContinue").hidden=true
+  document.getElementById("btnDoor").hidden=false
+  document.getElementById("btnSearch").hidden=false 
+  document.getElementById("btnDoor").hidden=false
+  document.getElementById("btnIdle").hidden=false
+  document.getElementById("btnDoorSearch").hidden=false
+    // let Bd=5
+    // let Fd=5
+    // let Fx=5
+    // let Cd=5
     let r = 0;
     let spareFuses = 3;
+    let cam=false
     let thunder = false;
     let fuseStatus = true;
     let emergencyFuse = true;
     let energy = 60
     let doorBtn = true;
-    let dev = false
+    let dev = true
     function door() { if (doorBtn && (fuseStatus || emergencyFuse)) { return true } else { return false } }
     let animatronics = [
       { name: "Bonnie", position: 0, path: 5, level: Bd, defaultLvl: Bd, overcharged: false, canAtk: false, status: true, rooms: ["Stage", "Dining Area", "Parts & Services", "Supply Closet", "Corridor", "Security Door"] },
@@ -62,6 +62,7 @@ async function main() {
       { name: "Foxy", position: 0, path: 4, level: Fx, defaultLvl: Fx, overcharged: false, canAtk: false, status: true, rooms: ["Pirate Cove", "Pirate Cove", "Pirate Cove", "Corridor", "Security Door"] },
       { name: "Chica", position: 0, path: 5, level: Cd, defaultLvl: Cd, overcharged: false, canAtk: false, status: true, rooms: ["Stage", "Dining Area", "Restrooms", "Kitchen", "Corridor", "Security Door"] },
     ];
+    let animaPosition=["BnP","FrP","FxP","ChP"]
     // console.clear()
   
     console.log(
@@ -81,9 +82,7 @@ async function main() {
       switch (clickedId) {
         case "btnSearch":
           if (energy > 0) {
-            for (let a = 0; a < animatronics.length; a++) {
-              console.log(animatronics[a].name + " " + animatronics[a].rooms[animatronics[a].position]);
-            }
+           cam=true
             energy--;
           } else {
             console.log("No energy to search them, Dangers lurk in the dark");
@@ -101,9 +100,7 @@ async function main() {
   
         case "btnDoorSearch":
           if (energy > 3) {
-            for (let a = 0; a < animatronics.length; a++) {
-              console.log(animatronics[a].name + ": " + animatronics[a].rooms[animatronics[a].position]);
-            }
+            cam=true
             doorBtn = true;
             energy -= 4;
           } else {
@@ -143,10 +140,30 @@ async function main() {
       for (let ac = 0; ac < animatronics.length; ac++) {
   
       }
+      if(cam){ 
+      for (let a = 0; a < animatronics.length; a++) {
+              console.log(animatronics[a].name + " " + animatronics[a].rooms[animatronics[a].position]);
+              document.getElementById(animaPosition[a]).textContent=animatronics[a].name+": "+ animatronics[a].rooms[animatronics[a].position]
+            }
+          }else{
+            for (let a = 0; a < animatronics.length; a++) {
+              console.log(animatronics[a].name + " " + animatronics[a].rooms[animatronics[a].position]);
+              document.getElementById(animaPosition[a]).textContent=animatronics[a].name+": Loading..."
+            }
+          }
       if (r >= 40) {
         console.log("You survived the night");
-        return;
+            document.getElementById("btnDoor").hidden=true
+            document.getElementById("btnStart").hidden=false
+            document.getElementById("btnContinue").hidden=false
+            document.getElementById("btnDoor").hidden=true
+            document.getElementById("btnSearch").hidden=true 
+            document.getElementById("btnIdle").hidden=true
+            document.getElementById("btnDoorSearch").hidden=true
+
+        return 1;
       }
+      //Useless in Html version, may end up active in console tho
       if (dev) {
   
         console.log("Round:" + r)
@@ -163,7 +180,7 @@ async function main() {
         console.log("Current Ia: Bn:" + iaEn[0] + " Fr:" + iaEn[1] + " Ch:" + iaEn[2] + " Fx" + iaEn[3])
   
       }
-  
+    //Thunder mechanic (broken?)
       if (!thunder) {
         if (Math.floor(Math.random() * 100 + 1) < 10) {
           console.log("A storm started...")
@@ -181,6 +198,8 @@ async function main() {
           console.log("Emergency fuse destroyed")
         }
       }
+
+      //Animatronics actions
       for (let aa = 0; aa < animatronics.length; aa++) {
         if (animatronics[aa].overcharged) {
           animatronics[aa].level--
@@ -191,10 +210,23 @@ async function main() {
         } else if (animatronics[aa].position >= animatronics[aa].path && Math.floor(Math.random() * 10 + 1) < 4) {
   
         } else if (animatronics[aa].position >= animatronics[aa].path && !door() && animatronics[aa].canAtk) {
+         //This one kinda important
+         
           if (Math.floor(Math.random() * 10 + 1) <= animatronics[aa].level) {
             console.clear()
+            document.getElementById("yourLife").textContent="You got jumpscare by " + animatronics[aa].name
             console.log("You got jumpscare by " + animatronics[aa].name);
-            return;
+            document.getElementById("jumpscare").play()
+            document.getElementById("animatronics").style.backgroundColor="rgba(99, 5, 5, 0.5)"
+            document.getElementById("btnDoor").hidden=true
+            document.getElementById("btnStart").hidden=false
+            document.getElementById("btnContinue").hidden=false
+            document.getElementById("btnDoor").hidden=true
+            document.getElementById("btnSearch").hidden=true 
+            document.getElementById("btnIdle").hidden=true
+            document.getElementById("btnDoorSearch").hidden=true
+
+            return 0;
           }
         } else if (
           animatronics[aa].position >= animatronics[aa].path &&
@@ -211,6 +243,8 @@ async function main() {
         ) {
           animatronics[aa].canAtk = true;
           console.log("Someones At the door")
+          document.getElementById("animatronicDoor").play()
+
         } else if (animatronics[aa].position > 1 && Math.floor(Math.random() * 100 + 1) < 15) {
           if (animatronics[aa].position > 2 && Math.floor(Math.random() * 100 + 1) < 10) {
             animatronics[aa].position--
@@ -244,9 +278,50 @@ async function main() {
       }
       r++;
       doorBtn = false;
+      cam=false
     }
   }
 
-
-
 main();
+async function fnafAlikeGame() {
+  console.clear()
+
+
+  while (true) {
+    console.log(
+      "What night you wanna play? \n1) First Night\n2) Second Nigth\n3) Third Night") 
+     // let option = readline.question("Input number: ");
+    const option=await waitForButtonClick(["btnStart","btnContinue"])
+    document.getElementById("yourLife").textContent=""
+    let diff=0
+    const nights=[[3, 1, 2, 1],[2, 5, 3, 4],[4,6,4,5],[7,2,1,6],[8,9,8,7]]
+
+
+    switch (option) {
+      case "btnStart":
+        diff=0
+        TNaF(nights[diff])
+        break;
+      case "btnContinue":
+        if(diff>4){
+          diff=4
+        }else{
+        diff+=TNaF(nights[diff])
+        }
+
+
+      break;
+      // case "3":
+      //   fnafAlike(5, 9, 8, 7,40);
+      //   break;
+
+      // case "4":
+      //   acessibility();
+      //   break    
+
+      case "0": return;
+
+
+    }}}
+    
+  
